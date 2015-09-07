49104882be85ef85c00be9b9efe80ca782b76ba4
Caleb Ely <le717.code@yahoo.com>
2015-09-07 17:09:51 -0400

    Update small image, add messages to batch script

--- Tools/post-install.bat
+++ Tools/post-install.bat
@@ -1,6 +1,7 @@
 @echo off
 
 rem Delete unnecessary files
+echo Deleting unnecessary files...
 del /F /Q LegoRR.*
 del /F /Q Autorun.exe Autorun.inf
 del /F /Q Data\Delme.dat Data\cd.key
@@ -9,6 +10,7 @@ rem Use the correct exe
 move EXE\LegoRR.* .\
 
 rem Delete unnecessary folders
+echo Deleting unnecessary folders...
 rd /S /Q DirectX6
 rd /S /Q Registration
 rd /S /Q EXE
