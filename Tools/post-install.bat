36708e7b140311cc47cb1f645da4209a1cc85d28
Caleb Ely <le717.code@yahoo.com>
2016-03-09 10:39:54 -0500

    Update post-install.bat

--- Tools/post-install.bat
+++ Tools/post-install.bat
@@ -1,17 +1,15 @@
 @echo off
 
-rem Delete unnecessary files
 echo Deleting unnecessary files...
 del /F /Q LegoRR.exe LegoRR.icd
 del /F /Q Autorun.exe Autorun.inf
 del /F /Q Data\Delme.dat Data\cd.key
 
-rem Use the correct exe
-echo Installing correct exe...
+rem Use the x86 exe
+echo Installing x86 exe...
 move EXE\LegoRR.exe .\
 move EXE\LegoRR.icd .\
 
-rem Delete unnecessary folders
 echo Deleting unnecessary folders...
 rd /S /Q DirectX6
 rd /S /Q Registration
